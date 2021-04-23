340119535bab64190d184a98ca12f08270bee749
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2021-04-24 08:59:43 +0900

    右ペインにサークルを表示

--- src/content.rs
+++ src/content.rs
@@ -1,4 +1,8 @@
-use iced::{text_input, Column, Text, TextInput};
+use iced::Container;
+use iced::{
+  canvas::{Canvas, Cursor, Frame, Geometry, Path, Program},
+  text_input, Color, Element, Length, Rectangle, TextInput,
+};
 
 use crate::gui::Message;
 
@@ -21,7 +25,7 @@ impl Content {
     }
   }
 
-  pub fn view(&mut self) -> Column<Message> {
+  pub fn view(&mut self) -> Element<Message> {
     let input = TextInput::new(
       &mut self.input_state,
       "This is the placeholder...",
@@ -29,13 +33,43 @@ impl Content {
       Message::TextInputChanged,
     )
     .padding(10);
+    let canvas = Canvas::new(Circle { radius: 50.0 });
 
     match self.position {
-      ContentState::Left => Column::new().push(input).padding(5).into(),
-      ContentState::Right => Column::new()
-        .push(Text::new("I <3 iced!"))
+      ContentState::Left => Container::new(input)
+        .width(Length::Fill)
+        .height(Length::Fill)
         .padding(5)
         .into(),
+      ContentState::Right => Container::new(canvas)
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .padding(20)
+        .center_x()
+        .center_y()
+        .into(),
     }
+  }
+}
+
+#[derive(Debug)]
+struct Circle {
+  radius: f32,
+}
+
+// Then, we implement the `Program` trait
+impl Program<Message> for Circle {
+  fn draw(&self, bounds: Rectangle, _cursor: Cursor) -> Vec<Geometry> {
+    // We prepare a new `Frame`
+    let mut frame = Frame::new(bounds.size());
+
+    // We create a `Path` representing a simple circle
+    let circle = Path::circle(frame.center(), self.radius);
+
+    // And fill it with some color
+    frame.fill(&circle, Color::BLACK);
+
+    // Finally, we produce the geometry
+    vec![frame.into_geometry()]
   }
 }
