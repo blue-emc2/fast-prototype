e4785e1479cf696e33b5845d83e127f4769d85c6
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2021-05-08 00:06:12 +0900

    2文字目がはてなだったらDecisionを返す

--- src/parser.rs
+++ src/parser.rs
@@ -3,8 +3,12 @@ use crate::node::NodeState;
 pub struct Parser {}
 
 impl Parser {
-  pub fn parse(_token: &str) -> Result<NodeState, String> {
-    Ok(NodeState::Action)
+  pub fn parse(token: &str) -> Result<NodeState, String> {
+    let chars = token.replace(" ", "").chars().collect::<Vec<char>>();
+    match chars[1] {
+      '?' => Ok(NodeState::Decision),
+      _ => Ok(NodeState::Action),
+    }
   }
 }
 
@@ -13,14 +17,20 @@ mod tests {
   use super::*;
 
   #[test]
-  fn test_node_state_action() {
+  fn test_node_state_action_from_hoge() {
     let state = Parser::parse("hoge");
     assert_eq!(state, Ok(NodeState::Action));
   }
 
   #[test]
-  fn test_node_state_decision_from_token() {
+  fn test_node_state_decision_from_a_b_c() {
     let state = Parser::parse("a ? b : c");
     assert_eq!(state, Ok(NodeState::Decision));
+  }
+
+  #[test]
+  fn test_node_state_action_from_A() {
+    let state = Parser::parse("A");
+    assert_eq!(state, Ok(NodeState::Action));
   }
 }
