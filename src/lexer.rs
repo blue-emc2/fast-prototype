bf4baa93e918eac6f5a7d379bac16a0f574b0821
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2021-05-08 17:18:45 +0900

    NodeStateをVectorで返すように変更
    トークンごとに分けるため

--- src/lexer.rs
+++ src/lexer.rs
@@ -3,16 +3,16 @@ use crate::node::NodeState;
 pub struct Lexer {}
 
 impl Lexer {
-  pub fn scan(token: &str) -> Result<NodeState, String> {
+  pub fn scan(token: &str) -> Result<Vec<NodeState>, String> {
     let chars = token.replace(" ", "").chars().collect::<Vec<char>>();
 
     if chars.len() == 1 {
-      return Ok(NodeState::Action);
+      return Ok(vec![NodeState::Action]);
     }
 
     match chars[1] {
-      '?' => Ok(NodeState::Decision),
-      _ => Ok(NodeState::Action),
+      '?' => Ok(vec![NodeState::Decision]),
+      _ => Ok(vec![NodeState::Action]),
     }
   }
 }
@@ -24,24 +24,24 @@ mod tests {
   #[test]
   fn test_node_state_action_from_hoge() {
     let state = Lexer::scan("hoge");
-    assert_eq!(state, Ok(NodeState::Action));
+    assert_eq!(state, Ok(vec![NodeState::Action]));
   }
 
   #[test]
   fn test_node_state_decision_from_a_b_c() {
     let state = Lexer::scan("a ? b : c");
-    assert_eq!(state, Ok(NodeState::Decision));
+    assert_eq!(state, Ok(vec![NodeState::Decision]));
   }
 
   #[test]
   fn test_node_state_action_from_a() {
     let state = Lexer::scan("A");
-    assert_eq!(state, Ok(NodeState::Action));
+    assert_eq!(state, Ok(vec![NodeState::Action]));
   }
 
   #[test]
   fn test_node_state_decision_from_abc_d_e() {
     let state = Lexer::scan("abc ? d : e");
-    assert_eq!(state, Ok(NodeState::Decision));
+    assert_eq!(state, Ok(vec![NodeState::Decision]));
   }
 }
